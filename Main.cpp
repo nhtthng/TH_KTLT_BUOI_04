8663e743abada0331193f03af31232f3113b56c7
nhat thang <nhtthng2812@gmail.com>
2024-07-31 16:55:41 +0700

    Finish Cau 1.3

--- Main.cpp
+++ Main.cpp
@@ -8,12 +8,19 @@ int Cau_1_1(int n) {
 	return n + Cau_1_1(n - 1);
 }
 
+float Cau_1_3(int n) {
+	if (n == 0) {
+		return 0;
+	}
+	return (n / n + 1) + (n - 1 / (n + 1) - 1);
+}
+
 int main() {
 	int n;
 	cout << "Nhap so luong: ";
 	cin >> n;
 	int result;
-	result = Cau_1_1(n);
+	result = Cau_1_3(n);
 	cout << "Ket qua la: " << result;
 	return 0;
 }
